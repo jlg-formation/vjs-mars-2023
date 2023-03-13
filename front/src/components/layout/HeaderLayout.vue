5ca31111af6a62f3a2a475fc18fbbd63f5657d94
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-13 17:10:30 +0100

    dev: started vuejs

---
+++ front/src/components/layout/HeaderLayout.vue
@@ -0,0 +1,10 @@
+<template>
+  <header>
+    <a href="#">
+      <img src="/public/logo.svg" alt="Logo Gestion Stock" />
+      <span>Gestion Stock</span>
+    </a>
+  </header>
+</template>
+
+<style scoped></style>
