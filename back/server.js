fa186697d726680fcda65abb063e30eb8fda8069
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-13 14:47:30 +0100

    dev: added api on web server

--- back/server.js
+++ back/server.js
@@ -1,13 +1,22 @@
 console.log("About to start a server");
 
 const express = require("express");
+const serveIndex = require("serve-index");
+const api = require("./api");
 
 const app = express();
 const port = 3000;
+const publicDir = ".";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req: ", req.method, req.url);
+  next();
 });
+
+app.use("/api", api);
+
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
