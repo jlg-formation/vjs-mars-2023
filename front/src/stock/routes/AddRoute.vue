111b75b49c828e419fc10c95a4a6a8b53573613e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-14 14:47:22 +0100

    dev: add route dom and scss ok

--- front/src/stock/routes/AddRoute.vue
+++ front/src/stock/routes/AddRoute.vue
@@ -1,7 +1,46 @@
 <template>
   <main>
-    <p>Add route</p>
+    <h1>Ajouter un article</h1>
+    <form>
+      <label>
+        <span>Nom</span>
+        <input type="text" />
+      </label>
+      <label>
+        <span>Prix</span>
+        <input type="number" />
+      </label>
+      <label>
+        <span>Quantité</span>
+        <input type="number" />
+      </label>
+      <button class="primary" type="submit">Ajouter</button>
+    </form>
   </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+form {
+  display: flex;
+  flex-flow: column;
+  gap: 0.5em;
+
+  width: 100%;
+  max-width: 25em;
+
+  label {
+    display: flex;
+    flex-flow: column;
+
+    input {
+      padding: 0.5em 1em;
+      border: 0.1em solid #bbb;
+      border-radius: var(--border-radius);
+    }
+  }
+
+  button {
+    margin-top: 3em;
+  }
+}
+</style>
