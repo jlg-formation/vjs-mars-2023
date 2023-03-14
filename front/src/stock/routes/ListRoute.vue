ab9182a5e3c3038ce92f2679eea7406840cc5507
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-14 14:26:41 +0100

    dev: added article list table dom and css

--- front/src/stock/routes/ListRoute.vue
+++ front/src/stock/routes/ListRoute.vue
@@ -1,8 +1,88 @@
 <template>
   <main>
-    <p>List route</p>
-    <RouterLink :to="$route.path + '/add'">Ajout</RouterLink>
+    <h1>Liste des articles</h1>
+    <div class="content">
+      <nav>
+        <button>Rafraîchir</button>
+        <RouterLink :to="$route.path + '/add'" class="button">Ajouter</RouterLink>
+        <button>Supprimer</button>
+      </nav>
+      <table>
+        <thead>
+          <tr>
+            <th class="name">Nom</th>
+            <th class="price">Prix</th>
+            <th class="qty">Quantité</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="name">Tournevis</td>
+            <td class="price">3.99 €</td>
+            <td class="qty">15</td>
+          </tr>
+          <tr>
+            <td class="name">Pelle</td>
+            <td class="price">7.50 €</td>
+            <td class="qty">8</td>
+          </tr>
+          <tr>
+            <td class="name">Marteau</td>
+            <td class="price">1.50 €</td>
+            <td class="qty">100</td>
+          </tr>
+          <tr>
+            <td class="name">Bêche</td>
+            <td class="price">16.00 €</td>
+            <td class="qty">50</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+div.content {
+  display: flex;
+  flex-flow: column;
+  gap: 1em;
+}
+
+nav {
+  display: flex;
+  gap: 0.3em;
+}
+
+table {
+  border: 0.1em solid #bbb;
+  td,
+  th {
+    padding: 0.5em 1em;
+  }
+
+  thead {
+    background-color: #bbb;
+  }
+
+  tbody {
+    cursor: pointer;
+    tr:nth-child(even) {
+      background-color: #eee;
+    }
+
+    .price,
+    .qty {
+      text-align: right;
+    }
+
+    tr:hover {
+      background-color: #ddd;
+    }
+
+    tr.selected {
+      background-color: #ccc;
+    }
+  }
+}
+</style>
