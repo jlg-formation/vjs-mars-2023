ea3e1fcb2eb5e023c95d7aa09992cc10466d4d5c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-14 10:20:27 +0100

    dev: header ok css and dom

--- front/src/components/layout/HeaderLayout.vue
+++ front/src/components/layout/HeaderLayout.vue
@@ -7,4 +7,34 @@
   </header>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+header {
+  height: 3em;
+  padding: 0 0.5em;
+  background-color: #eee;
+  display: flex;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 0.5em;
+
+    text-decoration: inherit;
+    color: inherit;
+
+    span {
+      font-size: 1.3em;
+      font-weight: bold;
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+
+img {
+  height: 2.5em;
+}
+</style>
