4ad45850e9613b7e7162c7a0496d0b7bd04d737e
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-03-14 12:00:20 +0100

    dev: added legal route content dom and scss

--- front/src/components/routes/LegalRoute.vue
+++ front/src/components/routes/LegalRoute.vue
@@ -1,6 +1,23 @@
 <template>
   <main>
-    <p>Legal component</p>
+    <h1>Mentions Légales</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum accusamus corrupti ad
+      cupiditate voluptatem deleniti omnis ipsum quo iusto. Dolore architecto incidunt corrupti
+      quasi at dignissimos eos temporibus quisquam quos.
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis voluptate commodi eum earum
+      obcaecati cum at. Nesciunt, odit quos. Harum voluptatum reprehenderit cumque doloribus magni
+      dolor laboriosam officiis sequi maiores qui odit quaerat et animi sunt eius laudantium, eaque
+      quod sapiente odio distinctio tenetur. Ullam libero fugiat ut ad adipisci molestias
+      recusandae? Architecto fugiat libero, necessitatibus voluptate voluptatibus non sint suscipit
+      deleniti veritatis similique sit! Explicabo doloribus veniam aliquid libero repudiandae.
+      Accusamus rerum dolor facere corporis fuga ipsum commodi, inventore, nulla eaque cum saepe
+      quae dignissimos unde, perferendis eos harum beatae aspernatur? Labore libero accusantium,
+      dolores earum illum veritatis commodi.
+    </p>
   </main>
 </template>
 
